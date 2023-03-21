e684f93968fb3e1c2da2cc0834881a0c66679fed
sayordebbarma <sayor.debbarma2021@vitbhopal.ac.in>
2023-03-22 00:17:35 +0530

    Initial Commit

---
+++ VideoPlayer/VideoPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoPlayerApp.swift
+//  VideoPlayer
+//
+//  Created by Sayor Debbarma on 22/03/23.
+//
+
+import SwiftUI
+
+@main
+struct VideoPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
