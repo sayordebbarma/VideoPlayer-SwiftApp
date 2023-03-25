51e3b7aa07c0a7b74b80389b3c175c985e7989df
sayordebbarma <sayor.debbarma2021@vitbhopal.ac.in>
2023-03-25 21:48:19 +0530

    Added pixels API

--- VideoPlayer/videoManager.swift
+++ VideoPlayer/videoManager.swift
@@ -11,6 +11,40 @@ enum Query: String, CaseIterable {
 case nature, people, animal, food, ocean, anime
 }
 
+class videoManager: ObservableObject {
+    @Published private(set) var videos: [Video] = []
+    @Published var selectedQuery: Query = Query.nature
+    
+    init() {
+        Task.init {
+            await findVideos(topic: selectedQuery)
+        }
+    }
+    
+    func findVideos(topic: Query) async {
+        do {
+            guard let url = URL(string: "https://api.pexels.com/videos/search?query=\(topic)&per_page=10&orientation=potrait") else { fatalError("URL missing")}
+            
+            var urlRequest = URLRequest(url: url)
+            urlRequest.setValue("YOUR_API_KEY", forHTTPHeaderField: "Authorization")
+            
+            let ( data, response ) = try await URLSession.shared.data(for: urlRequest)
+            
+            guard (response as? HTTPURLResponse)?.statusCode == 200 else { fatalError("fetching data error") }
+            
+            let decoder = JSONDecoder()
+            decoder.keyDecodingStrategy = .convertFromSnakeCase
+            let decodedData = try decoder.decode(ResponseBoby.self, from: data)
+            
+            self.videos = []
+            self.videos = decodedData.videos
+            
+        } catch {
+            print("Error fetching data from pixels api: \(error)")
+        }
+    }
+}
+
 struct ResponseBoby: Decodable {
     var page: Int
     var perPage: Int
